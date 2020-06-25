0e62d7546ec2229173b180664509764750a09015
Hyeongkyeong Seo <seohk316@gmail.com>
2020-06-25 12:13:51 +0900

    Create main.c

---
+++ main.c
@@ -0,0 +1,28 @@
+#include <string.h>
+void func(void);
+typedef struct sDATA {
+        int m_List[14][2];
+        int m_Cursor;
+} DATA;
+DATA g_A;
+int main() {
+        while(1){
+               func();
+        }
+}
+void func(void) {
+        int nCursor;
+        int i, j, k, l;
+        memset(&g_A.m_List[0][0], 0, sizeof(g_A.m_List));
+
+        nCursor = g_A.m_Cursor;
+        for (i = 0; i < 5; i++) {
+               j = (nCursor + i) % 14;
+               g_A.m_Cursor += 1;
+               // g_A.m_Cursor=(j+1)%14;
+        }
+
+        k = g_A.m_List[j][0];
+        l = g_A.m_List[j][1];
+
+}
